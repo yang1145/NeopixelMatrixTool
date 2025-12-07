85be02f81e78b19611dd30c7f9d15594e3f277a3
DESKTOP-BH39479\Administrator <1069653183@qq.com>
2025-12-07 10:44:09 +0800

    停止跟踪 out/ 文件夹

---
+++ temp.py
@@ -0,0 +1,6 @@
+# Python env   :               
+# -*- coding: utf-8 -*-        
+# @Time    : 2025/12/6 下午10:29   
+# @Author  : 李清水            
+# @File    : temp.py       
+# @Description :             
